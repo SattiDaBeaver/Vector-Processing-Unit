82a3eaa5507b99b60710e499761f14911c718566
SattiDaBeaver <saptarshitalukdar2305@gmail.com>
2025-08-12 15:19:45 -0400

    added all the module wires, added instruction struct

--- Modules/Tiny_FSM_Control/tiny_fsm_control.sv
+++ Modules/Tiny_FSM_Control/tiny_fsm_control.sv
@@ -10,9 +10,12 @@ module tiny_fsm_control #(
     parameter DATA_WIDTH    = 8;
     parameter MATRIX_SIZE   = 8;
     parameter ACC_WIDTH     = 32;
-    parameter DATA_WIDTH    = 8;
+    parameter ADDR_WIDTH = $clog2(MATRIX_SIZE);
+    parameter ACC_ADDR_WIDTH = $clog2(MATRIX_SIZE*MATRIX_SIZE);
+
     // DPRAM Parameters
     parameter DP_ADDR_WIDTH = 10;
+
     // UART Instruction Memory Loader Parameters
     parameter F_CLK         = 50_000_000;
     parameter BAUD          = 921_600;
@@ -25,32 +28,81 @@ module tiny_fsm_control #(
     //             Module Wires
     //======================================//
     // Systolic Module Wires
-    logic                                clk,
-    logic                                rst,
-    logic                                acc_rst,
-    logic                                acc_en,
-    logic                                shift_en_right,
-    logic                                shift_en_down,
+    logic                               clk;
+    logic                               rst;
+    logic                               acc_rst;
+    logic                               acc_en;
+    logic                               shift_en_right;
+    logic                               shift_en_down;
 
-    logic [ACC_ADDR_WIDTH-1:0]           addr_acc,
+    logic [ACC_ADDR_WIDTH-1:0]          addr_acc;
 
-    logic                                buffer_rst_top,
-    logic                                load_en_top,
-    logic                                swap_buffers_top,
-    logic [ADDR_WIDTH-1:0]               addr_top,
-    logic [DATA_WIDTH-1:0]               data_in_top,
+    logic                               buffer_rst_top;
+    logic                               load_en_top;
+    logic                               swap_buffers_top;
+    logic [ADDR_WIDTH-1:0]              addr_top;
+    logic [DATA_WIDTH-1:0]              data_in_top;
 
-    logic                                buffer_rst_left,
-    logic                                load_en_left,
-    logic                                swap_buffers_left,
-    logic [ADDR_WIDTH-1:0]               addr_left,
-    logic [DATA_WIDTH-1:0]               data_in_left,
+    logic                               buffer_rst_left;
+    logic                               load_en_left;
+    logic                               swap_buffers_left;
+    logic [ADDR_WIDTH-1:0]              addr_left;
+    logic [DATA_WIDTH-1:0]              data_in_left;
 
-    logic [DATA_WIDTH*MATRIX_SIZE-1:0]   data_out_flat_bottom,
-    logic [DATA_WIDTH*MATRIX_SIZE-1:0]   data_out_flat_right,
-    logic [ACC_WIDTH-1:0]                acc_out
+    logic [DATA_WIDTH*MATRIX_SIZE-1:0]  data_out_flat_bottom;
+    logic [DATA_WIDTH*MATRIX_SIZE-1:0]  data_out_flat_right;
+    logic [ACC_WIDTH-1:0]               acc_out;
 
-    // 
+    // Dual Port RAM Wires
+    logic                               we_a;
+    logic [DP_ADDR_WIDTH-1:0]           addr_a;
+    logic [DATA_WIDTH-1:0]              din_a;
+    logic [DATA_WIDTH-1:0]              dout_a;
+    logic                               we_b;
+    logic [DP_ADDR_WIDTH-1:0]           addr_b;
+    logic [DATA_WIDTH-1:0]              din_b;
+    logic [DATA_WIDTH-1:0]              dout_b;
+
+    // UART Instruction Memory Loader Wires
+    logic                               uart_rx;
+    logic                               uart_tx;
+    logic [$clog2(INSTR_DEPTH)-1:0]     rd_addr;
+    logic [INSTR_WIDTH-1:0]             rd_data;
+
+    //======================================//
+    //          FSM Control Logic
+    //======================================//
+
+    typedef struct packed {
+        logic           LOAD_LEFT;      // bit 31
+        logic           LOAD_TOP;       // bit 30
+        logic           SWAP_LEFT;      // bit 29
+        logic           SWAP_TOP;       // bit 28
+        logic           SHIFT_RIGHT;    // bit 27
+        logic           SHIFT_DOWN;     // bit 26
+        logic           LOAD_ACC;       // bit 25
+        logic           WRITE_ACC_OUT;  // bit 24
+        logic           WAIT_CYCLES;    // bit 23
+        logic           JUMP;           // bit 22 -- Subject to change
+        logic           CLR;            // bit 21
+        logic           NOP;            // bit 20 -- Subject to change
+
+        logic [1:0]     RESERVED;       // bits 19 down to 18 (2 bits)
+
+        logic [4:0]     FLAGS;          // bits 17 down to 13 (5 bits)
+        logic [12:0]    ADDR;           // bits 12 down to 0 (13 bits)
+    } instruction_t;
+
+    typedef enum logic [1:0] { 
+        IDLE,
+        FETCH,
+        DECODE,
+        WAIT
+    } fsm_state_t;
+
+    instruction_t   instruction;
+    fsm_state_t     state;
+
 
     //======================================//
     //        Module Instantiation
@@ -89,22 +141,22 @@ module tiny_fsm_control #(
 
     // Dual Port RAM Instantiation
     dp_ram #(
-        .DATA_WIDTH(DATA_WIDTH)
+        .DATA_WIDTH(DATA_WIDTH),
         .ADDR_WIDTH(DP_ADDR_WIDTH)
         ) DPRAM (
-        .clk(),
+        .clk(clk),
 
         // Port A
-        .we_a(),
-        .addr_a(),
-        .din_a(),
-        .dout_a(),
+        .we_a(we_a),
+        .addr_a(addr_a),
+        .din_a(din_a),
+        .dout_a(dout_a),
 
         // Port B
-        .we_b(),
-        .addr_b(),
-        .din_b(),
-        .dout_b()
+        .we_b(we_b),
+        .addr_b(addr_b),
+        .din_b(din_b),
+        .dout_b(dout_b)
     );
 
     // UART Instruction Memory Loader Instantiation
